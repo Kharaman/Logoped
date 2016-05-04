d410e2472a32c82890e881247afaa54931c8c051
KharamanVladimir <kharaman.vova@idev.bz>
2016-05-04 17:34:34 +0300

    speech_screen controller and model created

---
+++ application/controllers/Speech_screen.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: khara
+ * Date: 04.05.2016
+ * Time: 16:12
+ */
+class Speech_screen extends CI_Controller
+{
+
+}
---
+++ application/models/Speech_sreen_model.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Speech_sreen_model extends MY_model
+{
+    protected static $table = 'speech_screen';
+}
